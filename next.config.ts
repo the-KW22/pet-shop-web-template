185c28d649eb984cd96a496d1ef6aa5ecdf5bade
the-KW22 <the.kw22me@gmail.com>
2026-02-02 10:47:44 +0800

    fix production error

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? '/portfolio/pet-shop-web-template' : '',
-  assetPrefix: isProd ? '/portfolio/pet-shop-web-template' : '',
+  basePath: '/portfolio/pet-shop-web-template',
+  assetPrefix: '/portfolio/pet-shop-web-template',
   images: {
     unoptimized: true, // Required for static export
   },
